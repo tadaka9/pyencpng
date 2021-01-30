b61aeda2ed3ca37319258a7e469d3a8639c5273e
tadaka9 <trapdoorfunction@gmail.com>
2021-01-30 18:17:51 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,47 @@
+# PyEncPNG or PyEncrypted PNG
+
+A steganographic library to encrypt files and messages in PNG images using CSPRNG random generated pixel colors, shuffled charset for pybase64, and AES-256 with tag (anti-tamper support, and with Intel-NI support on Intel processors) to encrypt or decrypt data (full UTF-8 support).
+
+## BLOCKCHAIN INFOS:
+
+This is the base library to build the Laniakea ([Laniakea Supercluster](https://en.wikipedia.org/wiki/Laniakea_Supercluster)) blockchain (to be developed).
+
+## INSTALLATION
+
+Use the package manager [pip](https://pip.pypa.io/en/stable/) and [git](https://git-scm.com/) distributed version control to install EncPNG.
+<br>On Linux, MacOS, and Windows's Cygwin (or MinGW), GCC or Clang should be installed.</br>
+<br>On Windows, Microsoft C/C++ compiler (for Visual Studio) should be installed (to avoid errors like: "Unable to find vcvarsall.bat").</br>
+OpenSSL should be installed.
+## Installation instructions
+
+```bash
+# Make sure you have the latest versions of pip, setuptools and wheel installed
+python3 -m pip install --upgrade pip setuptools wheel
+git clone https://github.com/tadaka9/Laniakea.git
+cd Laniakea; cd pyencpng
+pip3 install -r requirements.txt
+python3 setup.py install
+```
+
+## USAGE
+```
+python3 encpng.py [ [ -dir or --directory ] OUTPUT_PATH [ -e or --encrypt ] STRING or FILE [ -p or --password ] PASSWORD ] or [ [ --dir or --directory ] OUTPUT_PATH [ -d or --decrypt ] FILE [ -p or --password ] PASSWORD ]
+```
+## EXAMPLES
+```python
+python3 encpng.py --dir out/dir/path --encrypt file.txt --password Password1 23!"£
+```
+```python
+# within Python 3, using library
+from encpng import 
+# with result of type <class 'PIL.Image.Image'>
+result = encrypt("String", "Password1 23!\"£")
+```
+```python
+python encpng.py --dir out/dir/path --decrypt 08a30930-ecdf-4f6a-9978-c274093d63e1.png --password Password1 23!"£
+```
+```python
+from encpngcli import decrypt
+# result type may vary depending on encrypted data
+result = decrypt("08a30930-ecdf-4f6a-9978-c274093d63e1.png", "Password1 23!\"£")
+```
---
+++ encpng-cli.py
@@ -0,0 +1,57 @@
+"""
+Author:      tadaka9
+Date:        2020-25-10
+Description: An encryption/decryption library for strings, hashes, files, using PNG images as container
+             (generated with a cryptographically secure random generator for RGB values),
+             and an AES-256 cryptographically secure algorithm to store data.
+
+Copyright (c) 2020 tadaka9
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+from argparse import ArgumentParser
+from encpng import EncPNG
+from sys import exit, stderr
+
+
+class Parser(ArgumentParser):
+    def error(self, message):
+        stderr.write("Error: %s\n" % message)
+        self.print_help()
+        exit(2)
+
+
+parser = Parser(add_help=True, allow_abbrev=True, description="A steganographic encryption/decryption tool")
+parser.add_argument("-d", "--decrypt", nargs = "+", metavar = "FILE", help = "Usage: [-d | --decrypt] FILE")
+parser.add_argument("-dir", "--directory", metavar = 'PATH', nargs = "+",
+                    help = "Usage: [-dir or --directory] PATH", required=False)
+parser.add_argument("-e", "--encrypt", nargs = "+", metavar = ("FILE or STRING", "FILE or STRING"),
+                    help = "Usage: [-e or --encrypt] FILE or STRING")
+parser.add_argument("-p", "--password", metavar = 'STRING',
+                    nargs = "+", help = "Usage: [-p or --password] STRING", required=True)
+args = parser.parse_args()
+if not args.decrypt and not args.encrypt and not args.directory:
+    parser.error("Error: arguments not specified")
+if args.decrypt:
+    EncPNG(" ".join(args.decrypt), " ".join(args.password), " ".join(args.directory)).decrypt()
+if args.encrypt:
+    EncPNG(" ".join(args.encrypt), " ".join(args.password), " ".join(args.directory)).encrypt()
